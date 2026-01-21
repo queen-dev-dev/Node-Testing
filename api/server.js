36aa8f946e6b2e6a815ccd4e05799e32d788b157
Devon <100230169@student.burnley.ac.uk>
2026-01-21 22:16:34 +0000

    Now checks for GET requests

--- api/server.js
+++ api/server.js
@@ -2,18 +2,29 @@ import http from 'http';
 const PORT = process.env.PORT;
 
 const server = http.createServer(function(req, res) {
+    try{
+        //check if GET request
+        if (req.method === 'GET') {
+            if (req.url === '/') {
+                res.writeHead(200, {'Content-Type': 'application/json'});
+                res.end(JSON.stringify({Message: 'Welcome to the server'})); // ends stream, can also send text with it
+            } else if (req.url === '/about') {
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.end('<h1>Welcome to da club</h1>');
+            } else {
+                res.writeHead(404, {'Content-Type': 'text/html'});
+                res.end("<h1>NOT FOUND</h1>");
+            }
+        } else{
+            throw new Error('Method not allowed');
+        }
+    } catch (error) {
+        res.writeHead(500, {'Content-Type': 'text/plain'});
+        res.end("SERVER ERROR");
+    }
     console.log(req.url);
     console.log(req.method);
-    if (req.url === '/') {
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        res.end(JSON.stringify({Message: 'server error'})); // ends stream, can also send text with it
-    } else if (req.url === '/about') {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end('<h1>Welcome to da club</h1>');
-    } else {
-        res.writeHead(403, {'Content-Type': 'text/plain'});
-        res.end("ERROR, INCORRECT ADDRESS");
-    }
+    
 
 
     //res.setHeader('Content-type', 'text/html')  //sets type of content - returns text or html
