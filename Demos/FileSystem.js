47ba4bd4e79a7ecd82a61adb72c507392778dbcd
Devon <100230169@student.burnley.ac.uk>
2026-01-22 20:50:48 +0000

    Early FS demo

--- Demos/FileSystem.js
+++ Demos/FileSystem.js
@@ -1,7 +1,15 @@
 import fs from 'fs';
 
 // readFile() - callback
-fs.readFile('./testing.txt', 'utf8', (err, data) => {
+fs.readFile('./Demos/testing.txt', 'utf8', (err, data) => {
     if (err) throw err;
     console.log(data);
 })
+
+// readFileSync() - Synchronous BLOCKING version
+
+const data = fs.readFileSync('./Demos/testing.txt', 'utf8');
+console.log(data);
+
+
+// Build command node Demos/FileSystem.js
