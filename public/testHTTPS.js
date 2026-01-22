c0c936fd7da6a6ac67975c5d03ef972a4eb68603
Devon <100230169@student.burnley.ac.uk>
2026-01-22 09:47:57 +0000

    Update testHTTPS.js

--- public/testHTTPS.js
+++ public/testHTTPS.js
@@ -1,10 +1,17 @@
-let xhr = new XMLHttpRequest();
-
-xhr.open("GET", 'https://node-testing-three.vercel.app/api/users');
-
-xhr.onload = function(params, otherparams, extraparams) {
-    console.log(params);
-    console.log(otherparams);
-    console.log(extraparams);
+async function getUsers() {
+    try{
+        let res = await fetch ("../api/users");
+        console.log(res.ok);
+        if (!res.ok) {
+            throw new Error("ruh roh get out of here");
+        }
+        let theJson = await res.json();
+        console.log(res);
+        console.log(theJson);
+    }
+    catch(error) {
+        console.error(error.message);
+    }
 }
-xhr.send();
+
+getUsers();
